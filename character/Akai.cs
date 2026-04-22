25e7a785a95f729c8a5b796b28da221cddcee137
huangjin-233 <h505077@163.com>
2026-04-22 12:42:33 +0800

    角色

--- character/Akai.cs
+++ character/Akai.cs
@@ -3,13 +3,14 @@ using BaseLibToRitsu.Generated;
 using Godot;
 using MegaCrit.Sts2.Core.Entities.Characters;
 using MegaCrit.Sts2.Core.Models;
+using STS2RitsuLib.Scaffolding.Characters;
 using YakumoAkai.character.card.basic;
 using YakumoAkai.character.relics;
 
 namespace YakumoAkai.character
 {
-	public class Akai : PlaceholderCharacterModel
-	{
+    public class Akai : PlaceholderCharacterModel, IModCharacterEpochTimelineRequirement
+    {
 		public const string energyColorName = "yakumoakai";
 		// 角色名称颜色
 		public override Color NameColor => new(0.5f, 0.5f, 1f);
@@ -18,9 +19,9 @@ namespace YakumoAkai.character
 
 		// 人物性别（男女中立）
 		public override CharacterGender Gender => CharacterGender.Masculine;
-
-		// 初始血量
-		public override int StartingHp => 80;
+        //public override bool RequiresEpochAndTimeline => false;
+        // 初始血量
+        public override int StartingHp => 80;
 
 		// 人物模型tscn路径。要自定义见下。
 		public override string CustomVisualPath => "res://scenes/creature_visuals/akai.tscn";
@@ -89,8 +90,10 @@ namespace YakumoAkai.character
 			ModelDb.Relic<GensokyoOnlineBad>(),
 	];
 
-		// 攻击建筑师的攻击特效列表
-		public override List<string> GetArchitectAttackVfx() => [
+        public bool RequiresEpochAndTimeline => throw new System.NotImplementedException();
+
+        // 攻击建筑师的攻击特效列表
+        public override List<string> GetArchitectAttackVfx() => [
 			"vfx/vfx_attack_blunt",
 		"vfx/vfx_heavy_blunt",
 		"vfx/vfx_attack_slash",
