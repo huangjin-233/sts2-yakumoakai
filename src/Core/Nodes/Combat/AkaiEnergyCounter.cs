e2866c1863a486ef4182ca7c4f2abb9477cd340f
huangjin-233 <h505077@163.com>
2026-06-19 20:08:51 +0800

    Delete src/Core/Nodes/Combat directory

--- src/Core/Nodes/Combat/AkaiEnergyCounter.cs
+++
@@ -1,6 +0,0 @@
-using System;
-using Godot;
-using MegaCrit.Sts2.addons.mega_text;
-using MegaCrit.Sts2.Core.Nodes.Combat;
-
-public partial class AkaiEnergyCounter : NEnergyCounter { }
--- src/Core/Nodes/Combat/CustomCreatureVisuals.cs
+++
@@ -1,7 +0,0 @@
-using Godot;
-using MegaCrit.Sts2.Core.Nodes.Combat;
-using System;
-
-public partial class CustomCreatureVisuals : NCreatureVisuals
-{
-}
